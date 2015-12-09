5b3f5a25b820fe2594ebe8f7c4171bf136e759e3
Shourai <10200748+Shourai@users.noreply.github.com>
2015-12-09 19:31:30 +0100

    updated mario.c

--- set_1/mario.c
+++ set_1/mario.c
@@ -6,17 +6,22 @@ int main(void)
   printf("Please give height: ");
   scanf("%d", &input);
   
-  for(int i = 0; i < input; i++)
+  while (input <= 0 || input > 23)
   {
-    for(int j = input; j > i + 1; j--)
-    {
-      printf(" ");
-    }
-    for(int k = 0; k != i + 2; k++)
-    {
-      printf("#");
-    }
-    printf("\n");
+    printf("Please give height between 0 and 23: ");
+    scanf("%d", &input);
   }
+    for(int i = 0; i < input; i++)
+    {
+      for(int j = input; j > i + 1; j--)
+      {
+        printf(" ");
+      }
+      for(int k = 0; k != i + 2; k++)
+      {
+        printf("#");
+      }
+      printf("\n");
+    }
 }
 
