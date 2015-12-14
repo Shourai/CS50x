ffe4a2c7778fec8c07941c7adc7390860e86bca7
Shourai <10200748+Shourai@users.noreply.github.com>
2015-12-14 23:33:28 +0100

    removed redundant /if/ check

--- set_2/vigenere.c
+++ set_2/vigenere.c
@@ -11,37 +11,36 @@ int main(int argc, char *argv[])
       return 1;
   }
 
-  if (argc == 2) {
-    // Ask user for input and save it to string
-    char string[100];
-    char keyword[100];
-    printf("Please give string: ");
-    scanf("%99[^\n]%*c", string);
+  // Ask user for input and save it to string
+  char string[100];
+  char keyword[100];
+  printf("Please give string: ");
+  scanf("%99[^\n]%*c", string);
 
-    int j = 0; // initialize k_j
-    int m = strlen(argv[1]);
+  int j = 0; // initialize k_j
+  int m = strlen(argv[1]);
 
-    // Since 'A' = 'a' = 0 and 'Z' = 'z' = 25
-    // we could aswell lowercase everything in the keyword
-    for (int i = 0; i < m; i++)
+  // Since 'A' = 'a' = 0 and 'Z' = 'z' = 25
+  // we could aswell lowercase everything in the keyword
+  // Additionally we assign 'a' = 0 ... 'z' = 25
+  for (int i = 0; i < m; i++)
+  {
+    keyword[i] = tolower(argv[1][i]) - 'a';
+  }
+
+  for (int i = 0, n = strlen(string); i < n; i++)
+  {
+    if (isalpha(string[i]))
     {
-      keyword[i] = tolower(argv[1][i]) - 'a';
+      char c = string[i] - 'a';
+      printf("%c", 'a' + (c + keyword[j % m]) % 26) ;    // 'a' = 97
+      j++;
     }
 
-    for (int i = 0, n = strlen(string); i < n; i++)
+    else
     {
-      if (isalpha(string[i]))
-      {
-        char c = string[i] - 'a';
-        printf("%c", 'a' + (c + keyword[j % m]) % 26) ;    // 'a' = 97, 'z' = 122
-        j++;
-      }
-
-      else
-      {
-        printf("%c", string[i]);
-      }
+      printf("%c", string[i]);
     }
-    return 0;
-  } 
-}
+  }
+  return 0;
+} 
